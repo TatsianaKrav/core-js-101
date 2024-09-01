ec8ad8b47b169f7beed8617ec3d606554a1feb3e
Татьяна Кравченко <tats.krauchanka@gmail.com>
2024-09-01 16:00:09 +0300

    feat: complete generateOdds

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -35,8 +35,9 @@ function findElement(arr, value) {
  *    2 => [ 1, 3 ]
  *    5 => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  const arr = new Array(len).fill(1);
+  return arr.map((item, index) => 1 + index * 2);
 }
 
 
