0e111d1f216870b3cc737bde22c8fa06d6b9982c
Татьяна Кравченко <tats.krauchanka@gmail.com>
2024-09-01 12:46:58 +0300

    feat: extractNameFromTemplate

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -65,8 +65,9 @@ function getStringFromTemplate(firstName, lastName) {
  *   'Hello, John Doe!' => 'John Doe'
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  const res = value.replace('Hello, ', '');
+  return res.slice(0, res.length - 1);
 }
 
 
