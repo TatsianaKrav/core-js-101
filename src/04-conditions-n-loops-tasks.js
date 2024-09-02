07d350206d8a60745298454b8f75d3d1d2ac7ac3
Татьяна Кравченко <tats.krauchanka@gmail.com>
2024-09-02 22:15:04 +0300

    feat: complete isTriangle

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -95,8 +95,9 @@ function getSumBetweenNumbers(n1, n2) {
  *   10,1,1   =>  false
  *   10,10,10 =>  true
  */
-function isTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isTriangle(a, b, c) {
+  const max = Math.max(a, b, c);
+  return (a + b + c - max) > max;
 }
 
 
