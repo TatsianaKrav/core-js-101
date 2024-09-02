0c6842e18f94f9c8f7296d20def9d65efedaca29
Татьяна Кравченко <tats.krauchanka@gmail.com>
2024-09-02 22:10:33 +0300

    feat: complete sortCitiesArray

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -434,8 +434,19 @@ function toStringList(arr) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  *    ]
  */
-function sortCitiesArray(/* arr */) {
-  throw new Error('Not implemented');
+function sortCitiesArray(arr) {
+  arr.sort((a, b) => {
+    if (a.country > b.country) return 1;
+    if (a.country < b.country) return -1;
+    return 0;
+  });
+  return arr.sort((a, b) => {
+    if (a.country === b.country) {
+      if (a.city > b.city) return 1;
+      if (a.city < b.city) return -1;
+    }
+    return 0;
+  });
 }
 
 /**
